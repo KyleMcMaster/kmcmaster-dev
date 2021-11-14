bc8717087a2ec36c5e60fc200abf8ab8bc0c43fc
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2021-11-13 16:17:48 -0800

    feat(v3): add mock header

--- app.vue
+++ app.vue
@@ -1,24 +1,13 @@
 
 <script lang="ts" setup>
-  import './assets/css/tailwind.css'
+import './assets/css/tailwind.css'
+import Header from './header.vue';
 </script>
 
 <template>
-<div class="grid grid-flow-col grid-rows-2 grid-cols-3 gap-4">
-  <div>
-    1
+  <Header />
+  <div class="grid grid-flow-col grid-rows-2 grid-cols-3 gap-4">
+    <div>1</div>
+    <div class="col-start-3">2</div>
   </div>
-  <div class="col-start-3">
-    2
-  </div>
-  <div>
-    3
-  </div>
-  <div>
-    4
-  </div>
-  <div class="row-start-1 col-start-2 col-span-2">
-    5
-  </div>
-</div>
 </template>
---
+++ header.vue
@@ -0,0 +1,10 @@
+<template>
+    <header class="w-full bg-black p-3 px-5 shadow-xl text-gray-400 flex">
+        <span class="text-2xl flex items-center">Kyle McMaster</span>
+        <div class="flex-grow"></div>
+        <span class="inline-flex px-2">blog</span>
+        <span class="inline-flex px-2">github</span>
+        <span class="inline-flex px-2">linkedin</span>
+        <span class="inline-flex px-2">twitter</span>
+    </header>
+</template>
